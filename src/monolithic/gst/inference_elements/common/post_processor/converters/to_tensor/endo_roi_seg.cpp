abd6ea8f158671e667605b846d447361d568bac9
ScottWang <scott.wang@intel.com>
2024-09-01 20:48:54 +0800

    Now processFrame_BuildSegmentationMaskTensor can build a tensor but need to do 1D to 2D conversion and dim setting.

--- src/monolithic/gst/inference_elements/common/post_processor/converters/to_tensor/endo_roi_seg.cpp
+++ src/monolithic/gst/inference_elements/common/post_processor/converters/to_tensor/endo_roi_seg.cpp
@@ -17,6 +17,10 @@
 using namespace post_processing;
 using namespace InferenceBackend;
 
+#define NANONET_IMAGE_WIDTH  256
+#define NANONET_IMAGE_HEIGHT 256
+
+
 TensorsTable EndoRoiSegConverter::convert(const OutputBlobs &output_blobs) const {
     ITT_TASK(__FUNCTION__);
     TensorsTable tensors_table;
@@ -36,8 +40,8 @@ TensorsTable EndoRoiSegConverter::convert(const OutputBlobs &output_blobs) const
 
 
             for (size_t frame_index = 0; frame_index < batch_size; ++frame_index) {
-                processFrame_RawCopy(blob, prefixed_layer_name, batch_size, frame_index, tensors_table);
-                processFrame_RawCopy(blob, prefixed_layer_name2, batch_size, frame_index, tensors_table);
+                //processFrame_RawCopy(blob, prefixed_layer_name, batch_size, frame_index, tensors_table);
+                processFrame_BuildSegmentationMaskTensor(blob, prefixed_layer_name2, batch_size, frame_index, tensors_table);
             }
         }
     } catch (const std::exception &e) {
@@ -46,7 +50,8 @@ TensorsTable EndoRoiSegConverter::convert(const OutputBlobs &output_blobs) const
     return tensors_table;
 }
 
-void EndoRoiSegConverter::processFrame_RawCopy(const OutputBlob::Ptr &blob, const std::string &prefixed_layer_name, size_t batch_size, size_t frame_index, TensorsTable &tensors_table) const {
+void EndoRoiSegConverter::processFrame_RawCopy(const OutputBlob::Ptr &blob, const std::string &prefixed_layer_name, size_t batch_size, size_t frame_index, TensorsTable &tensors_table) const 
+{
     GstStructure *tensor_data = BlobToTensorConverter::createTensor().gst_structure();
 
     CopyOutputBlobToGstStructure(blob, tensor_data, BlobToMetaConverter::getModelName().c_str(),
@@ -61,16 +66,40 @@ void EndoRoiSegConverter::processFrame_RawCopy(const OutputBlob::Ptr &blob, cons
 }
 
 
-void EndoRoiSegConverter::processFrame_BuildSegmentationMaskTensor(const OutputBlob::Ptr &blob, const std::string &prefixed_layer_name, size_t batch_size, size_t frame_index, TensorsTable &tensors_table) const {
+void EndoRoiSegConverter::processFrame_BuildSegmentationMaskTensor(const OutputBlob::Ptr &blob, const std::string &prefixed_layer_name, size_t batch_size, size_t frame_index, TensorsTable &tensors_table) const 
+{
+
+    size_t input_width = getModelInputImageInfo().width;
+    size_t input_height = getModelInputImageInfo().height;
+
+
     GstStructure *tensor_data = BlobToTensorConverter::createTensor().gst_structure();
 
-    CopyOutputBlobToGstStructure(blob, tensor_data, BlobToMetaConverter::getModelName().c_str(),
+    GstStructure *tensor = tensor_data ; // For easy naming 
+
+    CopyOutputBlobToGstStructure(blob, tensor, BlobToMetaConverter::getModelName().c_str(),
                                  prefixed_layer_name.c_str(), batch_size, frame_index);
 
-    // In different versions of GStreamer, tensors_batch are attached to the buffer in a different order.
-    // Thus, we identify our meta using tensor_id.
-    gst_structure_set(tensor_data, "tensor_id", G_TYPE_INT, safe_convert<int>(frame_index), NULL);
 
-    std::vector<GstStructure *> tensors{tensor_data};
+    gst_structure_set_name(tensor, "mask_endo");
+    gst_structure_set(tensor, "precision", G_TYPE_INT, GVA_PRECISION_FP32, NULL);
+    //gst_structure_set(tensor, "format", G_TYPE_STRING, "segmentation_mask", NULL);
+
+    GValueArray *data0 = g_value_array_new(2);
+    GValue gvalue = G_VALUE_INIT;
+    g_value_init(&gvalue, G_TYPE_UINT);
+
+    size_t masks_width = input_width;
+    size_t masks_height = input_height;
+    g_value_set_uint(&gvalue, safe_convert<uint32_t>(masks_height));
+    g_value_array_append(data0, &gvalue);
+    g_value_set_uint(&gvalue, safe_convert<uint32_t>(masks_width));
+    g_value_array_append(data0, &gvalue);
+    gst_structure_set_array(tensor, "dims", data0);
+    g_value_array_free(data0);
+
+
+    std::vector<GstStructure *> tensors{tensor};
     tensors_table[frame_index].push_back(tensors);
+
 }
